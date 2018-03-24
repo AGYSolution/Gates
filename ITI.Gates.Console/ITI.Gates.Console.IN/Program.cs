efbcc08b40c25b517d75e0639c6a994ee2647073
alanpebr <33680403+alanpebr@users.noreply.github.com>
2018-03-24 10:06:52 +0700

    Commit Solution

---
+++ ITI.Gates.Console/ITI.Gates.Console.IN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Gates.Console.IN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
